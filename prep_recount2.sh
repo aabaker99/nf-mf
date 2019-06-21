1ebb76c899f4c766aea2b6a8727b1e479f8e1978
Aaron <aabaker99@gmail.com>
2019-06-21 08:48:59 -0700

    update to fix desired shape of data and running directory

--- prep_recount2.sh
+++ prep_recount2.sh
@@ -2,5 +2,4 @@
 set -e
 wget -O recount2_PLIER_data.zip https://ndownloader.figshare.com/files/10881866
 unzip recount2_PLIER_data.zip
-cd recount2_PLIER_data
 docker run -it -v `pwd`:`pwd` -w `pwd` aabaker99/cogaps:latest Rscript prep_recount2.R
