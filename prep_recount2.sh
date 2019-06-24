509cb588edc4bb1e6ae287a309e6fb8ede7330fa
Aaron <aabaker99@gmail.com>
2019-06-24 11:57:25 -0700

    no longer need to transform data because CoGAPS wrapper can support RDS

--- prep_recount2.sh
+++ prep_recount2.sh
@@ -2,4 +2,4 @@
 set -e
 wget -O recount2_PLIER_data.zip https://ndownloader.figshare.com/files/10881866
 unzip recount2_PLIER_data.zip
-docker run -it -v `pwd`:`pwd` -w `pwd` aabaker99/cogaps:latest Rscript prep_recount2.R
+#docker run -it -v `pwd`:`pwd` -w `pwd` aabaker99/cogaps:latest Rscript prep_recount2.R
