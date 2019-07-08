af86c8bbff2ca94773a5aaf1314b5063ba85e88e
Aaron <aabaker99@gmail.com>
2019-07-08 09:13:20 -0700

    follow MultiPLIER procedure exactly except for z-score normalization

--- prep_data.R
+++ prep_data.R
@@ -1,39 +1,50 @@
 library(argparse)
 library(biomaRt)
 
+prep_recount_multiplier = function(rpkm_rds) {
+  # https://github.com/greenelab/rheum-plier-data/blob/978c37938383ff7adcadacfcbc35931ce5e62b17/recount2/2-prep_recount_for_plier.R
+  # normalized recount2 data
+  rpkm.df <- readRDS(rpkm_rds)
+
+  # set seed for reproducibility
+  set.seed(12345)
+
+  # Transform ensembl id to genesymbol
+  mart <- biomaRt::useDataset("hsapiens_gene_ensembl", 
+                              biomaRt::useMart("ensembl"))
+  genes <- unlist(lapply(strsplit(rpkm.df$ENSG, "[.]"), `[[`, 1))
+  rpkm.df$ensembl_gene_id <- unlist(lapply(strsplit(rpkm.df$ENSG, "[.]"), 
+                                           `[[`, 1))
+  gene.df <- biomaRt::getBM(filters = "ensembl_gene_id",
+                            attributes = c("ensembl_gene_id", "hgnc_symbol"),
+                            values = genes, 
+                            mart = mart)
+  # filter to remove genes without a gene symbol
+  gene.df <- gene.df %>% dplyr::filter(complete.cases(.))
+  # add gene symbols to expression df
+  rpkm.df <- dplyr::inner_join(gene.df, rpkm.df, 
+                               by = "ensembl_gene_id")
+  # set symbols as rownames (req'd for PLIER)
+  rownames(rpkm.df) <- make.names(rpkm.df$hgnc_symbol, unique = TRUE)
+  # remove gene identifier columns
+  rpkm.df <- rpkm.df %>% dplyr::select(-c(ensembl_gene_id:ENSG))
+
+  # PLIER prior information (pathways)
+  allPaths <- PLIER::combinePaths(bloodCellMarkersIRISDMAP, svmMarkers,
+                                  canonicalPathways)
+  cm.genes <- PLIER::commonRows(allPaths, rpkm.df)
+
+  # filter to common genes before row normalization to save on computation
+  rpkm.cm <- rpkm.df[cm.genes, ]
+  return(rpkm.cm)
+}
+
 # extract data frame my_df = readRDS(recount_data_prep_PLIER.RDS)
 # and save the my_df$rpkm.cm as a separate RDS file for CoGAPS
 parser = ArgumentParser()
-parser$add_argument('--plier', '-p', required=TRUE, help="Pre-processed RPKM data along with other PLIER parameters as an RDS")
 parser$add_argument('--rpkm', '-r', required=TRUE, help="Raw RPKM values in an RDS")
 parser$add_argument('--outdir', '-o', required=TRUE)
 args = parser$parse_args()
 
-# load MultiPLIER rows
-plier_data = readRDS(args$plier)
-rows_hgnc = row.names(plier_data$rpkm.cm)
-rm(plier_data)
-
-# raw rpkm data has row names in first column
-rpkm = readRDS(args$rpkm)
-row.names(rpkm) = rpkm[,1]
-rpkm = rpkm[,-1]
-
-# map ENSG in recount2 to HGNC
-ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl")
-bm_rv = getBM(
-  attributes = c("ensembl_gene_id_version", "hgnc_symbol"),
-  filters = c("ensembl_gene_id_version"),
-  values = row.names(rpkm),
-  mart = ensembl
-)
-
-# use biomaRt mapping to select rows used by MultiPLIER
-rows_df = data.frame(ensembl_gene_id_version = row.names(rpkm))
-rows_merged = merge(x = rows_df, y = bm_rv, all.x = TRUE, by.x = 'ensembl_gene_id_version', by.y = 'ensembl_gene_id_version')
-rows_filtered = rows_merged[rows_merged$hgnc_symbol %in% rows_hgnc,]
-saveRDS(rows_filtered, file.path(args$outdir, 'recount2_row_filter.RDS'))
-
-# rows are named by ENSG_id
-# filter and save
-saveRDS(rpkm[rows_filtered$ensembl_gene_id_version,], file.path(args$outdir, 'recount2_data_prep_CoGAPS.RDS'))
+rpkm.cm = prep_recount_multiplier(args$rpkm)
+saveRDS(rpkm.cm, file.path(args$outdir, 'recount2_data_prep_CoGAPS.RDS'))
