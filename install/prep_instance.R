171335c7e0adc9718e16c7ec840c567470029cc2
Aaron <aabaker99@gmail.com>
2019-07-19 09:36:49 -0700

    add more dependencies

--- install/prep_instance.R
+++ install/prep_instance.R
@@ -1,5 +1,11 @@
 #!/usr/bin/env Rscript
 install.packages("argparse")
 install.packages("BiocManager")
+install.packages("dplyr")
+requireNamespace("BiocManager")
+requireNamespace("devtools")
 BiocManager::install("CoGAPS")
 BiocManager::install("biomaRt")
+BiocManager::install("qvalue")
+BiocManager::install("recount")
+devtools::install_github("wgmao/PLIER", ref="afb4ccbf761418535c3e47ec6baeb6bcd8ec716a")
