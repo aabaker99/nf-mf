32df25e42fe7b91e52fe108c29bd7b46e963d10a
Aaron <aabaker99@gmail.com>
2019-07-30 13:54:26 -0700

    remove unused

--- install/prep_recount2.R
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env Rscript
-
-# convert RDS to csv
-rv = readRDS(file.path('recount2_PLIER_data', 'recount_rpkm.RDS'))
-write.csv(t(rv), file.path('recount2_PLIER_data', 'recount2_sample_by_gene.csv'), row.names=FALSE)
