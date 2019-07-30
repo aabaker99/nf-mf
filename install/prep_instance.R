ebef9de50040251a50c628aa0b43095107ca2eb5
Aaron <aabaker99@gmail.com>
2019-07-30 12:37:04 -0700

    add previous installation checks

--- install/prep_instance.R
+++ install/prep_instance.R
@@ -4,13 +4,34 @@ local({
   r["CRAN"] <- "http://cran.r-project.org" 
   options(repos=r)
 })
-install.packages("argparse")
-install.packages("BiocManager")
-install.packages("dplyr")
+if (!requireNamespace("argparse", quietly = TRUE)) {
+  install.packages("argparse")
+}
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
+if (!requireNamespace("dplyr", quietly = TRUE)) {
+  install.packages("dplyr")
+}
+if (!requireNamespace("devtools", quietly = TRUE)) {
+  install.packages('devtools')
+}
 requireNamespace("BiocManager")
 requireNamespace("devtools")
-BiocManager::install("CoGAPS")
-BiocManager::install("biomaRt")
-BiocManager::install("qvalue")
-BiocManager::install("recount")
-devtools::install_github("wgmao/PLIER", ref="afb4ccbf761418535c3e47ec6baeb6bcd8ec716a")
+
+if (!requireNamespace("CoGAPS", quietly = TRUE)) {
+  # TODO use repo instead?
+  BiocManager::install("CoGAPS")
+}
+if (!requireNamespace("biomaRt", quietly = TRUE)) {
+  BiocManager::install("biomaRt")
+}
+if (!requireNamespace("qvalue", quietly = TRUE)) {
+  BiocManager::install("qvalue")
+}
+if (!requireNamespace("recount", quietly = TRUE)) {
+  BiocManager::install("recount")
+}
+if (!requireNamespace("PLIER", quietly = TRUE)) {
+  devtools::install_github("wgmao/PLIER", ref="afb4ccbf761418535c3e47ec6baeb6bcd8ec716a")
+}
